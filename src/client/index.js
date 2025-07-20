130e964556880c5fe866dcfe4ba4b2522fede386
PastArizox <arizoxfr@gmail.com>
2025-07-20 18:20:46 +0200

    Added base of client code

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,16 @@
+import io from 'socket.io-client';
+
 console.log('Client-side code is running!');
+
+const socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected to server with ID:', socket.id);
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from server with ID:', socket.id);
+});
+
+const canvas = document.getElementById('canvas');
+const canvasContext = canvas.getContext('2d');
