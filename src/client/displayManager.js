e6445b2d14b8e8368a5f5c07a256583d1f643a4d
PastArizox <arizoxfr@gmail.com>
2025-07-20 18:21:08 +0200

    Added base of displayManager

---
+++ src/client/displayManager.js
@@ -0,0 +1,37 @@
+export default class DisplayManager {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.canvasContext = canvas.getContext('2d');
+        this.render = this.render.bind(this);
+    }
+
+    render() {
+        this.canvasContext.clearRect(
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height
+        );
+        this.canvasContext.save();
+
+        this.renderBackground('black');
+        this.renderPlayers([]);
+        this.renderFood([]);
+
+        requestAnimationFrame(this.render);
+    }
+
+    renderBackground(color) {
+        this.canvasContext.fillStyle = color;
+        this.canvasContext.fillRect(
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height
+        );
+    }
+
+    renderPlayers(players) {}
+
+    renderFood(blobs) {}
+}
--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
 import io from 'socket.io-client';
+import DisplayManager from './displayManager.js';
 
 console.log('Client-side code is running!');
 
@@ -14,3 +15,10 @@ socket.on('disconnect', () => {
 
 const canvas = document.getElementById('canvas');
 const canvasContext = canvas.getContext('2d');
+
+function update() {
+    // Update game state or animations here
+}
+
+requestAnimationFrame(new DisplayManager(canvas).render);
+setInterval(update, 1000 / 60);
