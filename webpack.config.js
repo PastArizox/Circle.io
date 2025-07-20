7793f1c6c2f12bf8859239ba0e67d14dc8327ade
PastArizox <arizoxfr@gmail.com>
2025-07-20 13:09:26 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+import { resolve } from "path";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: "./src/client/index.js",
+    output: {
+        path: resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+};
