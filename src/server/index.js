497638895d5feaa6364c64e3719d38332ad6f5f8
PastArizox <arizoxfr@gmail.com>
2025-07-21 21:24:34 +0200

    Added canvas & new player initialisation

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import ServerGameData from '../client/clientGameData.js';
 
 console.log('Server-side code is running!');
 
 const port = process.env.PORT || 8080;
+
+const canvasSize = {
+    width: 800,
+    height: 800,
+};
 
 const app = express();
 app.use(express.static('dist'));
@@ -16,10 +22,30 @@ const io = new Server(server);
 io.on('connection', (socket) => {
     console.log('A user connected with ID:', socket.id);
 
+    initializeCanvas(socket);
+    initializeNewPlayer(socket);
+
     socket.on('disconnect', () => {
         console.log('User disconnected with ID:', socket.id);
     });
 });
+
+function initializeCanvas(socket) {
+    socket.emit('canvasSize', canvasSize);
+}
+
+function initializeNewPlayer(socket) {
+    const newPlayer = new Player(
+        socket.id,
+        Math.random() * canvasSize.width,
+        Math.random() * canvasSize.height
+    );
+
+    ServerGameData.players.push(newPlayer);
+
+    socket.emit('currentPlayers', ServerGameData.players);
+    socket.broadcast.emit('newPlayer', newPlayer);
+}
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
